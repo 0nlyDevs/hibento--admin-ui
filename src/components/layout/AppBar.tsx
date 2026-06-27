582ebda35c86d3509974f73fe2ff0aaa2464b82c
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-27 19:10:46 +0300

    feat: use header-white-text.svg logo on right side of appbar

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -3,31 +3,19 @@ import { Box } from "@mui/material";
 
 export const AppBar = () => (
   <RAAppBar>
+    <Box sx={{ flex: 1 }} />
     <Box
       sx={{
         display: "flex",
         alignItems: "center",
-        gap: 1.5,
-        ml: 2,
+        mr: 1,
       }}
     >
       <img
-        src="/logo.svg"
+        src="/header-white-text.svg"
         alt="hibento"
-        style={{ height: 28, width: "auto" }}
+        style={{ height: 26, width: "auto" }}
       />
-      <Box
-        component="span"
-        sx={{
-          fontFamily: '"Sora", serif',
-          fontWeight: 700,
-          fontSize: "1.2rem",
-          letterSpacing: "-0.02em",
-          color: "#fafdf6",
-        }}
-      >
-        Bento.
-      </Box>
     </Box>
   </RAAppBar>
 );
