9967177d292a626a0a458bf8a130cedcfabfc253
colombefioren <colomberakotonjanahary@gmail.com>
2026-06-27 19:07:39 +0300

    style: add glass morphism and background transparency to layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,26 @@
 import { Layout as RALayout, CheckForApplicationUpdate } from "react-admin";
+import { GlobalStyles } from "@mui/material";
 import { Menu } from "./components/layout/Menu";
 import { AppBar } from "./components/layout/AppBar";
 
 export const Layout = ({ children }: { children: React.ReactNode }) => (
-  <RALayout appBar={AppBar} menu={Menu}>
-    {children}
-    <CheckForApplicationUpdate />
-  </RALayout>
+  <>
+    <GlobalStyles
+      styles={{
+        ".RaLayout-content": {
+          background: "transparent !important",
+        },
+        "#main-content": {
+          background: "transparent !important",
+        },
+        ".MuiPaper-root[class*=RaLayout]": {
+          background: "transparent !important",
+        },
+      }}
+    />
+    <RALayout appBar={AppBar} menu={Menu}>
+      {children}
+      <CheckForApplicationUpdate />
+    </RALayout>
+  </>
 );
