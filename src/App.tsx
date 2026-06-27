2cd805872425f5365debc262fe759a00ceee9fef
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-06-27 12:06:59 +0300

    feat: add Resource components with guessers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser, ShowGuesser } from "react-admin";
 import { Layout } from "./Layout";
 import { hibentoDataProvider } from "./providers";
 import { authProvider } from "./providers/authProvider";
@@ -8,5 +8,36 @@ export const App = () => (
     dataProvider={hibentoDataProvider}
     authProvider={authProvider}
     layout={Layout}
-  />
+  >
+    <Resource
+      name="events"
+      list={ListGuesser}
+      show={ShowGuesser}
+      recordRepresentation="title"
+    />
+    <Resource
+      name="sessions"
+      list={ListGuesser}
+      show={ShowGuesser}
+      recordRepresentation="title"
+    />
+    <Resource
+      name="speakers"
+      list={ListGuesser}
+      show={ShowGuesser}
+      recordRepresentation="name"
+    />
+    <Resource
+      name="rooms"
+      list={ListGuesser}
+      show={ShowGuesser}
+      recordRepresentation="name"
+    />
+    <Resource
+      name="venues"
+      list={ListGuesser}
+      show={ShowGuesser}
+      recordRepresentation="name"
+    />
+  </Admin>
 );
